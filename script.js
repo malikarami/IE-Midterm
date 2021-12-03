93475d9b54c5b77d37a06027d342203d67cf7b36
Maryam Alikarami <m.alikarami@yahoo.com>
2021-12-03 21:41:36 +0330

    adding coments for documentation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// in this section of code we access HTML elements and save them as objects
+// if necessary we add an eventListener to the elements
+const alert = document.getElementsByClassName("alert")[0];
+
 const submitButton = document.getElementsByClassName("submit-button")[0];
 submitButton.addEventListener('click', handleSubmit);
 
@@ -17,46 +21,120 @@ const resultGender = document.getElementsByClassName("result-gender")[0];
 const resultProbability = document.getElementsByClassName("result-probability")[0];
 const storageValue = document.getElementsByClassName("storage-value")[0];
 
+// this function is called when the submit button is clicked
 function handleSubmit(event){
+    // preventing the default behaviour of a submit button
     event.preventDefault();
+    // checking if the input is filled
+    if (!nameInput.value){
+        alert.innerHTML = "You must enter a name";
+        alert.style = "display: flex";
+        return;
+    }
+    // checking if the input value matches the space-or-character pattern
+    if(!/^[a-zA-Z_ ]*$/.test(nameInput.value)){
+        alert.innerHTML = "The name must contain only english characters and space";
+        alert.style = "display: flex";
+        return;
+    }
     sendRequest();
 }
 
+// this function is called when the clear button is clicked
 function handleClear(event) {
     event.preventDefault();
+    // removing the saved key from the local storage
     localStorage.removeItem(nameInput.value);
+    // setting the page state (no saved value => we don't display anything)
     storageValue.innerHTML = '';
 }
 
+// this function is called whenever the input's character changes
 function handleInputChange(){
-    console.log(nameInput.value);
+    // if the input matches any saved value we display that
     storageValue.innerHTML = JSON.parse(localStorage.getItem(nameInput.value)) || '';
+    // resetting the page to an initial state
     resultGender.innerHTML = '';
     resultProbability.innerHTML = '';
     femaleCheck.checked = false;
     maleCheck.checked = false;
+    // hiding the alert box
+    alert.innerHTML = 'alert message'
+    alert.style = "display: none";
 }
 
 function handleSave(event){
+    // preventing the default behaviour of a submit button
     event.preventDefault();
     const key = nameInput.value;
-    if (!key){
-        window.alert("You must enter a name");
+    // checking if the input value contains any characters
+    if (!key.replace(/ /g, "")){
+        // display a suitable alert
+        alert.innerHTML = "name is missing";
+        alert.style = "display: flex";
         return;
     }
+    // checking if the input value matches the space-or-character pattern
     if(!/^[a-zA-Z_ ]*$/.test(key)){
-        window.alert("The name must contain only name and space");
+        // display a suitable alert
+        alert.innerHTML = "The name must contain only english characters and space";
+        alert.style = "display: flex";
         return;
     }
+    // accessing the checked value (which gender is selected?)
     const gender = maleCheck.checked ? 'male' : (femaleCheck.checked ? 'female' : '');
+    // checking if the radio button is checked
     if (!gender){
-        window.alert("You must choose a gender");
+        // display a suitable alert
+        alert.innerHTML = "You must choose a gender";
+        alert.style = "display: flex";
         return;
     }
+    // save the checked value in the storage
     localStorage.setItem(key, JSON.stringify(gender));
+    // displaying the saved value on the page
     storageValue.innerHTML = gender;
 }
 
+function sendRequest(){
+    fetch(`https://api.genderize.io/?name=${nameInput.value}`).then(response => {
+        if(response.ok){
+            // parsing the response body
+            response.json().then(data => {
+                // the gender field is empty in the response body
+                if (!data.gender){
+                    // display suitable alert
+                    alert.innerHTML = `"${nameInput.value}" doesn't have any assigned gender`
+                    alert.style = "display: flex";
+                    return;
+                }
+                // display the result on the page
+                resultGender.innerHTML = data.gender;
+                resultProbability.innerHTML =`%${data.probability * 100}`;
+                displaySavedItem();
+            }).catch(error => {
+                // error on parsing the response
+                console.error(error);
+            })
+        }else {
+            // the response doesn't have an OK state
+            response.json().then(error => {
+                // display suitable alert
+                alert.innerHTML = `status code ${response.status}: ${error.error}`
+                alert.style = "display: flex";
+            });
+        }
+    }).catch(() => {
+        //the server didn't send any response back
+        // display suitable alert
+        alert.innerHTML = `Connection Error`
+        alert.style = "display: flex";
+    });
+}
+
+// this function is called when we receive a response
+// and displays the saved gender
+// (actually this function is unnecessary cause we display the saved gender on each input change)
 function displaySavedItem(){
     const savedGender = JSON.parse(localStorage.getItem(nameInput.value));
     if(!savedGender){
@@ -65,24 +143,4 @@ function displaySavedItem(){
         storageValue.innerHTML = savedGender;
     }
 }
-
-function sendRequest(){
-    fetch(`https://api.genderize.io/?name=${nameInput.value}`).then(response => {
-        if(response.ok){
-            response.json().then(data => {
-                resultGender.innerHTML = data.gender;
-                resultProbability.innerHTML = `%${data.probability * 100}`;
-                displaySavedItem();
-            }).catch(error => {
-                // error on parsing the response
-                console.error(error);
-            })
-        }else{
-            // the response has not met 200 status
-            const status = response.status;
-            console.log('status', status);
-        }
-    }).catch(error => console.error(error)); //the server didn't send any response back
-}
-
 
